7e35431483a3c60933d641ebb36ffad0b396c7ae
Victor-Fernandes <victor.fernandes.07@gmail.com>
2020-06-24 18:26:06 -0300

    desafio finalizado

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // setar o ip que aparece no console
+  baseURL: 'http://10.0.2.2:3333',
 });
 
 export default api;
